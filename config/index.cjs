4682a59edd9c6d58ca57911fb177ac7c2d52ce54
Huangjiayao <119399981+Huangjiayao@users.noreply.github.com>
2022-11-29 16:16:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GfKhraYCUcfcR2b8w4S8VX-iIdYueg2T8VV0SYbvsY8',
+      useTemplateId: 'O4WowzHU6SKnRNz-O34kTzp_fCE3LWjDbCgtOnher6A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-08',
       province: '山西',
