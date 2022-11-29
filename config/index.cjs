360cb2191b287fb7937c91b020b701c0b8b5efe5
Huangjiayao <119399981+Huangjiayao@users.noreply.github.com>
2022-11-29 16:29:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,7 +20,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '昶',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
+      id: 'o8aHm6Dr4Avbooco0fG1z9i0IqEg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '5zPwuxGWkOiCtofm8NYYpQ9xwTbB-lk5AuVoZY_vRNI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -43,7 +43,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '欣',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
+      id: 'o8aHm6EICez0tFxty-APCKwMzDCs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'BDEwoG8tQvGXgEaqCVcUUZhPXvj_Wy_fxAAMlyoyubY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -72,7 +72,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
+      id: 'o8aHm6ONizJUDfrgUoVjvepoaJEY',
     }
   ],
 
