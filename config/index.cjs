fc2ae44d02a6886e0038f51018939b6ea9097c20
Huangjiayao <119399981+Huangjiayao@users.noreply.github.com>
2022-11-29 15:50:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,56 +8,70 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxe8dea72de3685dd8',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '51a5b50b5f411766decc5201650612a7',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '昶',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'C8NmUETc12w_ViFSIetJ9ubX34OznjKJEvuYDJgdwcU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '05-08',
+      province: '山西',
+      city: '太原',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        { type: '生日', name: '昶', year: '2004', date: '05-08', },
+        { type: '节日', name: '相识纪念日', year: '2022', date: '10-28', },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        //{ keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '欣',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'EMbzx7Fr7JwTAvDjnFSn5FyrSRY1kiiIBEiYRlE7yR4',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '08-11',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        { type: '生日', name: '欣', year: '2003', date: '08-11', },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        //{ keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    }
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Misrc4H7Zl_vFmafwjnYlukZYdWci9M6APcuVW9CcRM',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o8aHm6E8X-x2htP8-Sh9SHL0jDdI',
     }
   ],
 
